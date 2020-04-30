0c5c98d65abced6b7caa0d70ac4360aae30f9c31
Lucas Soares <lsoares267@gmail.com>
2020-04-29 21:37:34 -0300

    Upload project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editttttt <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
     </div>
-  );
+  ); // <div class="App" />
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>Chocolateeeeeee</p>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
